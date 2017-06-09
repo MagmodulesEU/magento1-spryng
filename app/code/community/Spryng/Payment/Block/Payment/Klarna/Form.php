ffc2391dea25711b8cbe560d4875b9a4cb5b0bf1
Magmodules <info@magmodules.eu>
2017-06-09 08:45:44 +0200

    Date of Birth

--- app/code/community/Spryng/Payment/Block/Payment/Klarna/Form.php
+++ app/code/community/Spryng/Payment/Block/Payment/Klarna/Form.php
@@ -15,4 +15,15 @@ class Spryng_Payment_Block_Payment_Klarna_Form extends Mage_Payment_Block_Form
         parent::_construct();
         $this->setTemplate('spryng/form/klarna.phtml');
     }
+
+    /**
+     * @return Mage_Core_Model_Abstract
+     */
+    public function getQuote()
+    {
+        $quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
+        $quote = Mage::getModel('sales/quote')->load($quoteId);
+        return $quote;
+    }
+
 }
